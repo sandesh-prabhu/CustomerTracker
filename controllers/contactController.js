1a4ade8e5a4083f7d64d20c78e4fc2bac53f8780
Sandesh Prabhu <sandeshprabhu2000@gmail.com>
2024-06-14 13:33:16 +0530

    both email and phone exits in single doc or doesnt even exists separtely or one exists and other doesnt exists

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -57,6 +57,79 @@ const addContact = async (req, res) => {
         contact: data,
       });
     }
+
+    // Both email and phoneNumber exists in request
+    else {
+      // If both email and phoneNumber exists in single document -> get data
+      let contact;
+      contact = await ContactModel.findOne({ email, phoneNumber });
+      if (contact) {
+        const data = await getCustomerData(contact);
+        return res.send({
+          success: true,
+          message: "",
+          contact: data,
+        });
+      }
+
+      // Get contact with the input email sorted by createdAt to get oldest record
+      const emailContact = await ContactModel.findOne({ email }).sort({
+        createdAt: 1,
+      });
+      // Get contact with the input phoneNumber sorted by createdAt to get oldest record
+      const phoneNumberContact = await ContactModel.findOne({
+        phoneNumber,
+      }).sort({ createdAt: 1 });
+
+      // If both doesnt even exists separtely -> create a primary -> get data
+      if (!emailContact && !phoneNumberContact) {
+        const contact = await new ContactModel({
+          phoneNumber,
+          email,
+        }).save();
+
+        const data = await getCustomerData(contact);
+        return res.send({
+          success: true,
+          message: "",
+          contact: data,
+        });
+      }
+
+      // If one exists and other doesnt exists -> create a secondary -> get data
+      if (
+        (emailContact && !phoneNumberContact) ||
+        (!emailContact && phoneNumberContact)
+      ) {
+        // Get Primary ID
+        let linkedId;
+        if (emailContact) {
+          linkedId =
+            emailContact?.linkPrecedence == "primary"
+              ? emailContact._id
+              : emailContact.linkedId;
+        } else {
+          linkedId =
+            phoneNumberContact?.linkPrecedence == "primary"
+              ? phoneNumberContact._id
+              : phoneNumberContact.linkedId;
+        }
+        // Create secondary contact
+        const contact = await new ContactModel({
+          phoneNumber,
+          email,
+          linkedId,
+          linkPrecedence: "secondary",
+        }).save();
+
+        const data = await getCustomerData(contact);
+        return res.send({
+          success: true,
+          message: "",
+          contact: data,
+        });
+      }
+    }
   } catch (error) {
     console.error(error);
     return res
